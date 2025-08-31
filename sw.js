4fa2948ba0595f4f26436b67028d2ff6f48d4547
daoum2020 <daoum2020@outlook.be>
2025-09-01 00:09:12 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e=>{self.skipWaiting()});
-self.addEventListener('activate', e=>{clients.claim()});
-// basic offline fallback
-self.addEventListener('fetch', event=>{
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).catch(()=>caches.match('/offline.html')));
-  }
-});
