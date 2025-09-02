c74d9e03e78a42c6200c7e21d9c42003112dece1
daoum2020 <daoum2020@outlook.be>
2025-09-02 21:26:50 +0200

    Delete .github/workflows directory

--- .github/workflows/config.js
+++
@@ -1,9 +0,0 @@
-window.PM_CONFIG = {
-  apiKey: "AlzaSyBIAJIfsh_cj-qUtqDYwjLAdDMcnTsLUpU",
-  authDomain: "protegemoi-ec5a9.firebaseapp.com",
-  projectId: "protegemoi-ec5a9",
-  storageBucket: "protegemoi-ec5a9.appspot.com",
-  messagingSenderId: "690907366325",
-  appId: "1:690907366325:ios:c316da54297222dc220ffc",
-  vapidKey: "" // à compléter si tu veux les notifications Web Push
-};
